14e2cee18510999b4e3e1bba3e8035247b3845c6
ankiyong <pop2413@gmail.com>
2022-03-07 17:36:25 +0900

    2022-03-07

---
+++ python/python_es.py
@@ -0,0 +1,20 @@
+from elasticsearch import Elasticsearch
+
+es = Elasticsearch('http://localhost:9200')
+
+doc = {"name": "kim", "age": 35}
+
+res = es.index(index='test_index',body=doc)
+print(res)
+
+doc = {
+        "size": 1,
+        "query": {
+          "term": {
+            "DestCityName": "Seoul"
+          }
+        }
+      }
+
+res = es.search(index='kibana_sample_data_flights', body=doc)
+print(res)
