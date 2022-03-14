da5dac49141f59155a9dec0af71adfa3a554420e
ankiyong <pop2413@gmail.com>
2022-03-14 17:27:29 +0900

    2022-03-14

--- python/text_crawl.py
+++ python/text_crawl.py
@@ -1,8 +1,20 @@
+from urllib import response
+import urllib.request
+import json
 import requests
-from bs4 import BeautifulSoup
 
-url = "http://hangul.thefron.me/"
+key = "4174677349706f70373455597a785a"
+url = f"http://openapi.seoul.go.kr:8088/{key}/json/TbPublicWifiInfo/1/5/"
+
 res = requests.get(url)
-soup =BeautifulSoup(res.text,"html.parser")
-print(soup)
 
+r_dict = json.loads(response.text)
+print(r_dict)
+
+
+
+
+# res = urllib.request.urlopen(url)
+# json_str = res.read().decode('utf-8')
+# data = json.loads(json_str)
+# print(json.dumps(data, indent=4, ensure_ascii=False))
