6b6e30a1af1efe53e9aa44b6a26b917d0c2e5d48
ankiyong <pop2413@gmail.com>
2022-03-30 16:50:51 +0900

    2022-03-30

--- python/text_crawl.py
+++ python/text_crawl.py
@@ -3,8 +3,10 @@ import requests
 
 url = "https://news.daum.net/breakingnews/society?page=1"
 res = requests.get(url)
-soup = BeautifulSoup(res.text,'html.parser')
-print(soup.find("ul",{"class" : "list_news2 list_allnews"}).find_all("li"))
+soup = BeautifulSoup(res.text,'html.parser').find("ul",{"class" : "list_news2 list_allnews"}).find_all("li")
+for a in soup:
+    print(soup.find("a"))
+
 # for num in range(1,101):
 #     url = f"https://news.daum.net/breakingnews/society?page={num}"
     
