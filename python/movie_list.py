aa5695d1323e8999530fa7cc0be44181265ff0fc
ankiyong <pop2413@gmail.com>
2022-04-05 16:43:30 +0900

    2022-04-05

--- python/movie_list.py
+++ python/movie_list.py
@@ -1,37 +1,7 @@
-from this import d
-import pandas as pd
-import requests
-from bs4 import BeautifulSoup
-import pprint
-# param_set = {"key" : "f75bc744a2d412f7ace5e5e133f74279",
-#               "openStartDt" : 2000,
-#               "itemPerPage" : 100000}
-# key= "f75bc744a2d412f7ace5e5e133f74279"
-# url = "https://kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json?key=f75bc744a2d412f7ace5e5e133f74279&openStartDt=2017&itemPerPage=100"
+a = input("입력")
+# print(type(a))
 
-# res = requests.get(url)
-# contents = res.text
-# print(contents['movieListResult'])
-
-# info = json.loads(contents)
-# df = pd.json_normalize(info)
-
-
-# df = pd.read_csv('C:/Users/pop24/Desktop/source_code/python/movie.csv',engine='python',sep=',',quotechar='"',error_bad_lines=False)
-# print(df.columns)
-
-
-def divide(x, y):
-    try:
-        result = x / y
-    except ZeroDivisionError:
-        print("0으로 나눌 수 없어요!")
-        return False
-    else:
-        print("결과:", result)
-        return True
-    finally:
-        print("나누기 연산을 종료합니다.")
-
-
-divide(4,2)
+if "^" in a:
+    print("^^^^^")
+else:
+    print("종료")
--- python/python_es.py
+++ python/python_es.py
@@ -1,20 +1,5 @@
-from elasticsearch import Elasticsearch
-
-es = Elasticsearch('http://localhost:9200')
-
-doc = {"name": "kim", "age": 35}
-
-res = es.index(index='test_index',body=doc)
-print(res)
-
-doc = {
-        "size": 1,
-        "query": {
-          "term": {
-            "DestCityName": "Seoul"
-          }
-        }
-      }
-
-# res = es.search(index='kibana_sample_data_flights', body=doc)
-# print(res)
+a = input('ㅇㅇ')
+if "^" in a:
+   print(a)
+else:
+  print("^$%")
