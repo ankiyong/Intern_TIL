d6bc44160e0d190dfd1553a86f8ec36302646ba7
ankiyong <pop2413@gmail.com>
2022-04-08 08:51:26 +0900

    readme 추가

--- python/elasticsearch/main.py
+++
@@ -1,6 +0,0 @@
-from re import search
-from functions import *
-es = Elasticsearch("localhost:9200")
-# create_index(es,"template","mapping")
-search_template(es,"search_template")
-search_data(es,"index_test")
